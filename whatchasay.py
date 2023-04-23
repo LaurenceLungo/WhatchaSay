b0b3b88e382f77d71cdcb10a95ca9a71d272400d
Laurence Ng <npn5000@gmail.com>
2023-04-23 15:03:34 +0800

    new package version compatability refractor

--- whatchasay.py
+++ whatchasay.py
@@ -9,6 +9,8 @@ from argparse import ArgumentParser
 from time import time
 from hanziconv import HanziConv
 from sklearn.metrics import silhouette_score
+from sklearn.utils._testing import ignore_warnings
+from sklearn.exceptions import ConvergenceWarning
 
 
 def pre_process(text):
@@ -20,16 +22,16 @@ def pre_process(text):
     pp_text = " ".join(vocabs)
     return pp_text
 
-
+@ignore_warnings(category=ConvergenceWarning)
 def find_opt_k(dat, feat):
-    K = list(np.linspace(200, dat.size, 10, dtype=int))
+    K = list(np.linspace(2, dat.size, 50, dtype=int))
     score_list = []
     k_list = []
     max_silhouette = [-1, 0]
     dec_trend = 0
 
     for k in K:
-        clusterer = KMeans(n_clusters=k, random_state=0)
+        clusterer = KMeans(n_clusters=k, random_state=0, n_init='auto')
         preds = clusterer.fit_predict(feat)
         score = silhouette_score(feat, preds)
         score_list.append(score)
@@ -37,12 +39,12 @@ def find_opt_k(dat, feat):
         print("For k = {}, silhouette score is {}".format(k, score))
         if score > max_silhouette[0]:
             max_silhouette = [score, k]
+            dec_trend = 0
         else:
             dec_trend += 1
-            if dec_trend == 3:
-                print("--- Found optimal k:", max_silhouette[1], "---")
-                print()
-                return max_silhouette[1]
+            if dec_trend == 10:
+                break
+    print("--- Found optimal k:", max_silhouette[1], "---\n")
     return max_silhouette[1]
 
 
@@ -58,20 +60,20 @@ args_value = vars(args)
 tdf = pd.read_csv(args_value['dataset'])
 stopwords = [' ', '\n', 'previous', 'message']
 vec = TfidfVectorizer(stop_words=stopwords, preprocessor=pre_process)
-vec.fit(tdf.Enquiry.values)
+vec.fit(tdf.values)
 for (k, v) in vec.vocabulary_.items():
     if len(k) == 1:
         print(k, ": ", v)
-features = vec.transform(tdf.Enquiry.values)
+features = vec.transform(tdf.values)
 
-cls = KMeans(n_clusters=find_opt_k(tdf, features), random_state=0)
+cls = KMeans(n_clusters=find_opt_k(tdf, features), random_state=0, n_init='auto')
 cls.fit(features)
 prediction = cls.predict(vec.transform([pre_process(args_value["test"])]))
 
 output_str = "\"" + args_value['test'] + "\"/"
 for i in range(len(cls.labels_)):
     if cls.labels_[i] == prediction:
-        output_str += ("\"" + tdf.Enquiry.values[i] + "\"/")
+        output_str += ("\"" + tdf.values[i][0] + "\"/")
 output_str = output_str.replace('\n', ' ')
 file = open('output.txt', 'w')
 file.write(output_str)
@@ -80,13 +82,14 @@ file.close()
 print("Output:", output_str)
 print("Running time:", time() - start_time, "seconds")
 
-if args.v:
-    pca = PCA(n_components=2, random_state=0)
-    reduced_features = pca.fit_transform(features.toarray())
 
-    # reduce the cluster centers to 2D
-    reduced_cluster_centers = pca.transform(cls.cluster_centers_)
+pca = PCA(n_components=2, random_state=0)
+reduced_features = pca.fit_transform(features.toarray())
 
-    plt.scatter(reduced_features[:, 0], reduced_features[:, 1], c=cls.predict(features))
-    plt.scatter(reduced_cluster_centers[:, 0], reduced_cluster_centers[:, 1], marker='x', s=150, c='b')
-    plt.show()
+# reduce the cluster centers to 2D
+reduced_cluster_centers = pca.transform(cls.cluster_centers_)
+
+plt.scatter(reduced_features[:, 0], reduced_features[:, 1], c=cls.predict(features))
+plt.scatter(reduced_cluster_centers[:, 0], reduced_cluster_centers[:, 1], marker='x', s=150, c='b')
+# plt.show()
+plt.savefig('clusters.png')
